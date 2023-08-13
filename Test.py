e2233185d75ae3b1f20cd1f53d830af0d2af408f
kemingzhang <3161557275@qq.com>
2023-08-13 22:42:56 +0800

    complex_import

--- Test.py
+++ Test.py
@@ -86,3 +86,7 @@ def updateOne(host,port,db_name,username,password,collection_name,filter,update)
     result = collection.update_one(filter,{"$set":update})
 #qwq
 #qwqwqwqw
+qwq=[]
+qwq.append('1')
+qwq.append('1')
+print(qwq)
